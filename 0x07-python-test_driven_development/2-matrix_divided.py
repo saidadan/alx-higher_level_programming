a68100d23fbb05da604e4cd4d45e1ca5c2b7e4a5
saidadan <saidadan05@gmail.com>
2024-10-08 14:52:47 -0400

    python test driven development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,59 @@
 #!/usr/bin/python3
+"""
+This is the divide module.
+has a function to divide matrix
+Divide all elements of a matrix
+"""
+
+
 def matrix_divided(matrix, div):
-    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    if not matrix:
-        raise TypeError(errorMessage)
+    """matrix must be a list of lists of integers/floats
+    Returns a new matrix
+    """
+    newmatrix = []
+    length = 0
+
+    # Divides all elements of a matrix
+    if isinstance(div, int) is False and isinstance(div, float) is False:
+        raise TypeError('div must be a number')
+    # Matrix must be a list of integers or floats, TypeError
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    if not isinstance(matrix[0], list):
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
     if not isinstance(matrix, list):
-        raise TypeError(errorMessage)
-    for lists in matrix:
-        if not isinstance(lists, list):
-            raise TypeError(errorMessage)
-        for item in lists:
-            if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(errorMessage)
-    for lists in matrix:
-        if len(lists) == 0:
-            raise TypeError(errorMessage)
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-    if not all(len(lists) == len(matrix[0]) for lists in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(item / div, 2) for item in lists] for lists in matrix]
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    # matix has to exist, can't be less or equal to 0
+    if len(matrix[0]) <= 0:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    # 1.let's create new matrix with newrow
+    for row in matrix:
+        newrow = []
+        # matrix must be a list
+        if type(row) is not list:
+            raise TypeError('matrix must be a matrix (list of lists) '
+                            'of integers/floats')
+        # 2. row is empty
+        if length is 0:
+            length = len(row)
+        # Each row must be the same size, TypeError
+        elif len(row) is not length:
+            raise TypeError('Each row of the matrix must have the same size')
+        # 3. each item has to be an integer or float
+        for item in row:
+            if type(item) is not int and type(item) is not float:
+                raise TypeError('matrix must be a matrix (list of lists) '
+                                'of integers/floats')
+            # 4. add content to the row
+            # elements will be divided by div and rounded with 2 decimal
+            # round() function returns a floating point number that is
+            # a rounded version of the specified number
+            # with the specified number of decimals
+            newrow.append(round(item / div, 2))
+        # add content to the matrix
+        newmatrix.append(newrow)
+    return newmatrix
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,42 @@
 #!/usr/bin/python3
-"""text indent"""
+"""
+text identation module:
+indents text
+useful
+"""
 
 
 def text_indentation(text):
-    """text indent"""
-    if not isinstance(text, str):
+    """text must be a string, There should be no space
+    at the beginning
+    or at the end of each printed line.
+    """
+    # text is a string, otherwise raise a TypeError
+    if isinstance(text, str) is False:
         raise TypeError("text must be a string")
 
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+    # if text is None or not isinstance(text, str) or len(text) < 0:
+    # raise TypeError('must be a string')
+    # string.replace(old, new, count)
+    # prints a text with 2 new lines
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    # 1) cut the entire line with \n
+    # 2) cut by separator with \n
+    # to the line that was strip (for.,?)
+    # loop text and split where "\n" is)
+    # end =""no space output promp line)
+    # there should be no spaces at the beginning
+    # and the end, line strip metodo
 
+    print("\n".join([line.strip() for line in text.split("\n")]), end="")
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    # if isinstance(text, str) is False:
+    # raise TypeError("text must be a string")
+    # delim = ":.?"
+    # for x in delim:
+    # text = text.replace(x, "{}\n\n".format(x))
+    # text = text.replace('\\', '')
+    # text = "\n".join([line.strip() for line in text.split('\n')])
+    # print(text, end="")
